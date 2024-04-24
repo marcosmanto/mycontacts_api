3195de4961813f625a510d86b2ecdaec66a2bbcf
Marcos Mantovani <mantovani.marcos@gmail.com>
2024-04-23 23:37:29 -0300

    Add remainders API endpoints

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,10 +1,10 @@
+const { response } = require("express");
 const ContactsRepository = require("../repositories/ContactsRepository");
 
 class ContactController {
   async index(req, res) {
     // List all records
     const contacts = await ContactsRepository.findAll();
-
     res.json(contacts);
   }
 
@@ -21,12 +21,59 @@ class ContactController {
     res.json(contact);
   }
 
-  store() {
+  async store(req, res) {
     // Create a record
+    const { name, email, phone, category_id } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return res.status(400).json({ error: "This e-mail is already in use" });
+    }
+
+    const contact = await ContactsRepository.create(
+      name,
+      email,
+      phone,
+      category_id,
+    );
+
+    res.json(contact);
   }
 
-  update() {
+  async update(req, res) {
     // Edit a record
+    const { id } = req.params;
+    const { name, email, phone, category_id } = req.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const contactEmailExists = await ContactsRepository.findByEmail(email);
+
+    if (contactEmailExists && contactEmailExists.id !== id) {
+      return res.status(400).json({ error: "This e-mail is already in use" });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    res.json(contact);
   }
 
   async delete(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const router = Router();
 router.get("/contacts", ContactController.index);
 router.get("/contacts/:id", ContactController.show);
 router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts", ContactController.store);
+router.put("/contact/:id", ContactController.update);
 
 module.exports = router;
